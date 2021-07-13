6a4f90aa84aea34740a144bf842e43d22e6132ee
Ryan Ballantine <rgballantine@gmail.com>
2021-07-13 16:20:24 +0200

    TodoList completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+import TaskTable from './components/TaskTable';
+import AddTask from './components/AddTask';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    let tasksString = localStorage.getItem('tasks');
+    tasksString = tasksString ? tasksString : '[]';
+    const tasks = JSON.parse(tasksString);
+
+    this.state = { tasks: tasks };
+  }
+
+  saveTasksState(tasks) {
+    this.setState({ tasks: tasks });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  onTaskCreated(task) {
+    this.state.tasks.push(task);
+    this.saveTasksState(this.state.tasks);
+  }
+
+  onTaskUpdated(task) {
+    const updatedTaskArr = this.state.tasks.map(t => t.id === task.id ? task : t);
+    this.saveTasksState(updatedTaskArr);
+  }
+
+  onTaskRemoved(taskId) {
+    const updatedTaskArr = this.state.tasks.filter(task => task.id !== taskId);
+
+    this.saveTasksState(updatedTaskArr);
+  }
+  
+  render() {
+    return (
+      <div className="container card mt-5 p-4">
+        <div className="text-center">
+          <h1>Todo List</h1>
+        </div>
+
+        <hr />
+
+        <div className='text-center'>
+          <h3>Our simple Todo List</h3>
+        </div>
+
+        <AddTask
+          createTask={(task) => this.onTaskCreated(task)}
+        />
+
+        <TaskTable
+          tasks={this.state.tasks}
+          taskUpdated={(task) => this.onTaskUpdated(task)}
+          taskRemoved={(taskId) => this.onTaskRemoved(taskId)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/models/Task.js
@@ -0,0 +1,8 @@
+
+export default class Task {
+    constructor(name) {
+        this.id = new Date().getTime();
+        this.completed = false;
+        this.name = name;
+    }
+}
